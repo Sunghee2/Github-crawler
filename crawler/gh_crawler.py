e87c85ec6f10f61a3fcf692a1f80d1392d8c4134
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2018-11-03 14:53:23 +0900

    Get last id of csv file :sparkles:

--- crawler/gh_crawler.py
+++ crawler/gh_crawler.py
@@ -2,21 +2,17 @@
 import json, requests
 import pandas
 import csv
-from pandas.io.json import json_normalize
 
 def get_last_id():
-    df = pandas.read_csv('sample.csv', usecols = ['id'])[-2:-1]
-    last = df.at[df.index.item(), 'id']
+        df = pandas.read_csv('sample.csv', usecols = ['id'])[-1:]
+        last_id = df.at[df.index.item(), 'id']
     # last = df.tail(1).index.item() 마지막 인덱스 리턴
-    return last
+    return last_id
     
 
 def main():
-    print(get_last_id())
-    r = requests.get("https://api.github.com/users")
     data = json.loads(r.text)
     df = pandas.read_json(r.text)
-    df.to_csv('sample.csv')
 
 if __name__ == "__main__":
     main()

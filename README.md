2832a8c7b37cb432a64fcc9df8ec644f0d581e81
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2018-11-03 22:01:00 +0900

    Get remaining limit

--- README.md
+++ README.md
@@ -32,13 +32,13 @@
 - [x] pip, requests 설치 —> url을 잘못알아서,,,여기까지 모두 의미 없었음ㅎ.. 삽질
 - [x] vs code python 설정
 
-> `https://api.github.com/users` 에서 아이디 개수가 불규칙적으로 나오는 것 같음ㅠ
+> - `https://api.github.com/users` 에서 아이디 개수가 불규칙적으로 나오는 것 같음ㅠ
 >
-> `https://api.github.com/users?since=` -> api 설명에도 *The integer ID of the last User that you've seen.*
+> - `https://api.github.com/users?since=` -> api 설명에도 *The integer ID of the last User that you've seen.*
 >
-> -> 여기는 location 안나옴
+>   ​	-> 여기는 location 안나옴
 >
-> -> 따로 `https://api.github.com/users/[username]` 해야 보임...
+>   ​		-> 따로 `https://api.github.com/users/[username]` 해야 보임...
 
 > star : `https://api.github.com/users/Sunghee2/starred`
 >
@@ -56,32 +56,38 @@
 - [x] /user/sunghee2 user상세정보 요청
 - [x] series => df
 - [x] 두 개의 df join하기 
+- [x] user상세정보 api -> web scraping (취소)
+- [ ] 
 
 > :bug: pandas 2.* 버전은 안됨.. 
 >
 > ​	-> pip3 사용, `#!/usr/bin/env python3` 맨 앞에 작성
 >
-> pandas에 to_csv 함수 : http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_csv.html?highlight=csv#pandas.DataFrame.to_csv
+> - pandas에 to_csv 함수 : http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_csv.html?highlight=csv#pandas.DataFrame.to_csv
 >
-> csv 전체를 읽어오는데 마지막 것만 읽어올 수 없을까…. -> index 
+> - csv 전체를 읽어오는데 마지막 것만 읽어올 수 없을까…. -> index 
 >
-> 맨 처음 실행했을 때(csv파일이 없을 때) 에러처리
+> - 맨 처음 실행했을 때(csv파일이 없을 때) 에러처리
 >
-> 마지막 index 받아오는 것 index 잘못함[-2:-1] 이렇게 하면 마지막에서 2번째 출력됨 -> [-1:]
+> - 마지막 index 받아오는 것 index 잘못함[-2:-1] 이렇게 하면 마지막에서 2번째 출력됨 -> [-1:]
 >
-> csv 파일 추가되는데 df의 row index가 그대로 들어감... 상관없나..? 일단 냅두자.. -> csv파일이랑 다르게 인덱스 제대로 나옴 -> df의 row index 없앰
+> - csv 파일 추가되는데 df의 row index가 그대로 들어감... 상관없나..? 일단 냅두자.. -> csv파일이랑 다르게 인덱스 제대로 나옴 -> df의 row index 없앰
 >
-> *API rate limit exceeded* -> oauth/ params에 client_id, client_secret 보내주면 됨
+> - *API rate limit exceeded* -> oauth/ params에 client_id, client_secret 보내주면 됨
 >
-> ​	(인증되었으면 시간당 5000, 인증x면 시간당 60) https://api.github.com/rate_limit : 남아있는 요청 횟수
+>   ​	(인증되었으면 시간당 5000, 인증x면 시간당 60) https://api.github.com/rate_limit : 남아있는 요청 횟수
 >
-> *ValueError: If using all scalar values, you must pass an index* -> `read_json(typ='series')`
+> - *ValueError: If using all scalar values, you must pass an index* -> `read_json(typ='series')`
 >
 > <br/>
 >
-> api 호출 너무 많아서 user details은 web에서 들고오자……….ㅜ -> 근데 api보다 엄청 느린 듯 ㅠㅠㅠㅠ
+> - api 호출 너무 많아서 user details은 web에서 긁어오자……….ㅜ -> 근데 api보다 엄청 느린 듯 ㅠㅠㅠㅠ
 >
-> *Command "python setup.py egg_info" failed with error code 1 in /private/tmp/pip-install-lgil0nds/BeautifulSoup/*   --> 코드만 보고 BeautifulSoup로 다운받았는데 알고보니 bs4로 install 했어야 했다....
+>   ​	xxxxxxxx api는 제한이 있다 해도 30명당 20초인데 scraping은 한 명당 50초 가까이 걸림 ==> 다시 변경 
+>
+>   ​	star은 또 api를 이용해야하니깐 starred는 scraping으로 하자 -> 그냥 starred는 따로 파일 만들자...
+>
+> - *Command "python setup.py egg_info" failed with error code 1 in /private/tmp/pip-install-lgil0nds/BeautifulSoup/*   --> 코드만 보고 BeautifulSoup로 다운받았는데 알고보니 bs4로 install 했어야 했다....
 
 > crawler 참고 코드
 >
--- crawler/gh_crawler.py
+++ crawler/gh_crawler.py
@@ -23,63 +23,44 @@ def add_user_details(df):
             'followers', 'following',
             'created_at', 'updated_at'
     ])
-    for html_url in df['html_url']:
-        r = requests.get(html_url)
-        html = r.text
-        soup = BeautifulSoup(html, 'html.parser')
-        name = soup.find('span', class_='p-name vcard-fullname d-block overflow-hidden')
-        name.text if name is not None else ""
-        location = soup.find('span', class_='p-label')
-        print(location)
-    #     s = pandas.read_json(r.text, typ='series')[[
-    #         'id', 'name', 'company',
-    #         'blog', 'location', 'email',
-    #         'public_repos', 'public_gists', 
-    #         'followers', 'following',
-    #         'created_at', 'updated_at'
-    #     ]]
-    #     df_details = df_details.append(s, ignore_index=True)
-    # return df.join(df_details.set_index('id'), on='id')
+    for login in df['login']:
+        r = requests.get("https://api.github.com/users/" + login, params={
+            'client_id': client_id,
+            'client_secret': client_secret
+        })
+        s = pandas.read_json(r.text, typ='series')[[
+            'id', 'name', 'company',
+            'blog', 'location', 'email',
+            'public_repos', 'public_gists', 
+            'followers', 'following',
+            'created_at', 'updated_at'
+        ]]
+        df_details = df_details.append(s, ignore_index=True)
+    return df.join(df_details.set_index('id'), on='id')
 
-# def add_user_details(df):
-#     df_details = pandas.DataFrame(columns=[
-#             'id', 'name', 'company',
-#             'blog', 'location', 'email',
-#             'public_repos', 'public_gists', 
-#             'followers', 'following',
-#             'created_at', 'updated_at'
-#     ])
-#     for login in df['login']:
-#         r = requests.get("https://api.github.com/users/" + login, params={
-#             'client_id': client_id,
-#             'client_secret': client_secret
-#         })
-#         s = pandas.read_json(r.text, typ='series')[[
-#             'id', 'name', 'company',
-#             'blog', 'location', 'email',
-#             'public_repos', 'public_gists', 
-#             'followers', 'following',
-#             'created_at', 'updated_at'
-#         ]]
-#         df_details = df_details.append(s, ignore_index=True)
-#     return df.join(df_details.set_index('id'), on='id')
+def get_remaining_limit():
+    r = requests.get("https://api.github.com/rate_limit", params={
+            'client_id': client_id,
+            'client_secret': client_secret
+    })
+    return r.json()["rate"]["remaining"]
 
 
 def main():
     last_id = get_last_id()
+    # while()
     r = requests.get("https://api.github.com/users", params={
             'since': last_id, 
             'client_id': client_id,
             'client_secret': client_secret
     })
     df = pandas.read_json(r.text)
-    add_user_details(df)
-    # df_join = add_user_details(df)
-    # print(df.tail(1).index.item())
-    # if last_id == 0:
-    #     df_join.to_csv('sample.csv', index=False)
-    # else:
-    #     df_join.to_csv('sample.csv', mode='a', header=False, index=False)
+    df_join = add_user_details(df)
+    if last_id == 0:
+        df_join.to_csv('sample.csv', index=False)
+    else:
+        df_join.to_csv('sample.csv', mode='a', header=False, index=False)
+    last_id = df.tail(1).index.item()
 
 if __name__ == "__main__":
-    main()
+    get_remaining_limit()
